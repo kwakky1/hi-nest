43393afce6e1cfe3f4f6454520429abaa303971e
andy <kwakky1@gmail.com>
2021-01-11 00:41:01 +0900

    how to create test code in NestJs

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Req, Res } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
@@ -11,11 +11,15 @@ export class MoviesService {
     return this.movies;
   }
   getOne(id: number): Movie {
-    return this.movies.find((movie) => movie.id === id);
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
-  deleteOne(id: number): boolean {
-    this.movies.filter((movie) => movie.id !== id);
-    return true;
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
   }
   create(movieData: CreateMovieDto) {
     this.movies.push({
